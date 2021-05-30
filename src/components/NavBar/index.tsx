7795ecbf6749b1bcc5de615714716b04a1d98e1c
Den4200 <dennis@dennispham.me>
2021-05-30 12:56:33 -0400

    Create initial navigation bar.

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import logo from "../../logo.png";
+
+const navigation = [
+  { name: "Introduction", path: "#" },
+  { name: "Analytics and Data", path: "#" },
+  { name: "Privacy", path: "#" },
+  { name: "Other things", path: "#" },
+];
+
+const NavBar = () => (
+  <Disclosure as="nav" className="bg-gray-800">
+    {({ open }) => (
+      <>
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="sr-only">Menu</span>
+                {open ? (
+                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <Link className="flex items-center" to="/">
+                <img className="block h-8 w-auto" src={logo} alt="Workflow" />
+                <p className="hidden lg:block ml-2 text-base font-bold text-white">
+                  User Analytics and Data
+                </p>
+              </Link>
+
+              <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.path}
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile menu */}
+        <Disclosure.Panel className="sm:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                to={item.path}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
+);
+
+export default NavBar;
