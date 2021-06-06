316e5d207b37fcf077257191a5d30220f54071a7
Den4200 <dennis@dennispham.me>
2021-06-06 15:25:36 -0400

    Create inital user analytics and data page.
    - Added hero image
    - Created layout

--- src/pages/AnalyticsAndDataPage/index.tsx
+++ src/pages/AnalyticsAndDataPage/index.tsx
@@ -2,6 +2,18 @@ const AnalyticsAndDataPage = () => (
   <div className="content-container mx-auto px-8 mt-8">
     <h1 className="text-3xl font-bold mb-4">User Analytics and Data</h1>
     <hr />
+    <img src="https://blog.addthiscdn.com/wp-content/uploads/2018/06/19143007/What-are-Web-Analytics-The-Data-You-Need-to-Succeed.png" alt="" />
+    <hr className="mb-4" />
+
+    <h2 className="text-2xl font-bold mb-2">Analytics</h2>
+    <p className="text-lg">
+      Stuff about analytics!
+    </p>
+
+    <h2 className="text-2xl font-bold mt-8 mb-2">Data</h2>
+    <p className="text-lg">
+      Stuff about data!
+    </p>
   </div>
 );
 
