c94e0aec5d91a843ffc34477274600525ed2860b
Den4200 <dennis@dennispham.me>
2021-06-07 07:59:53 -0400

    Add hero image for privacy page.

--- src/pages/PrivacyPage/index.tsx
+++ src/pages/PrivacyPage/index.tsx
@@ -1,7 +1,11 @@
+import privacyImage from "./privacy.png";
+
 const PrivacyPage = () => (
   <div className="content-container mx-auto px-8 mt-8">
     <h1 className="text-3xl font-bold mb-4">Privacy</h1>
     <hr />
+    <img src={privacyImage} alt=""/>
+    <hr className="mb-4"/>
   </div>
 );
 
