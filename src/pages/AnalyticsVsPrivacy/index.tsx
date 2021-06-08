deabf9eb1841429a83a7b5f9a4e58ecf79cceaf3
Den4200 <dennis@dennispham.me>
2021-06-08 08:18:35 -0400

    Add content to the analytics and data vs. privacy page.

--- src/pages/AnalyticsVsPrivacy/index.tsx
+++ src/pages/AnalyticsVsPrivacy/index.tsx
@@ -6,6 +6,25 @@ const AnalyticsVsPrivacy = () => (
     <hr />
     <img src={analyticsVsPrivacyImage} alt="" />
     <hr className="mb-4" />
+
+    <h2 className="text-2xl font-bold mb-2">Privacy Laws and Regulations</h2>
+    <p className="text-lg">
+      Although it is true that user privacy is still breached a lot today despite the great
+      advances in privacy mentioned before, new laws are also being put into effect to combat those
+      breaches and protect user privacy. The many privacy breaches and rising demand of consumers
+      for privacy and control of their own data "have led governments to adopt new regulations,
+      such as the General Data Protection Regulation (GDPR) in Europe and the California Consumer
+      Privacy Act (CCPA) in that U.S. state" (Anant).
+    </p>
+
+    <p className="text-lg mt-4 mb-8">
+      These laws and regulations will help to ensure that user data and privacy are protected.
+      Going back to an earlier point, this will only drive innovation and privacy forward together,
+      as companies will have to implement privacy protections directly within their technologies in
+      order to follow these laws. This, in turn, will help companies build trust with their
+      customers, bringing them more business. Both sides of the spectrum will pull each other
+      forward.
+    </p>
   </div>
 );
 
